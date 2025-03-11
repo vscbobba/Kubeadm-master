6e8281bfeeb8910af7c15fb1b747501afbf2e348
vscbobba <59333292+vscbobba@users.noreply.github.com>
2025-03-12 00:07:29 +0530

    Update kubeadm-worker.tf

--- Application/kubeadm-worker.tf
+++ Application/kubeadm-worker.tf
@@ -38,7 +38,10 @@ resource "aws_instance" "kubeadm-worker" {
       "sudo systemctl restart systemd-hostnamed",
       "sudo mkdir -p /mnt/data/jenkins",
       "sudo chown -R 1000:1000 /mnt/data/jenkins",
-      "sudo chmod -R 775 /mnt/data/jenkins"
+      "sudo chmod -R 775 /mnt/data/jenkins",
+      "sudo mkdir -p /mnt/data/sonarqube/{data,logs,extensions} && \",
+      "sudo chown -R 1000:1000 /mnt/data/sonarqube && \",
+      "sudo chmod -R 775 /mnt/data/sonarqube"
     ]
 
     connection {
